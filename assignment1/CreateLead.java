129f5a5875b86a17cd63ac7158b4c791faa19906
GeethaShan <88589335+GeethaShan@users.noreply.github.com>
2021-09-11 11:48:25 +0530

    Add files via upload
    assignments.week5.day1

---
+++ assignment1/CreateLead.java
@@ -0,0 +1,34 @@
+package assignments.week5.day1.assignment1;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass {
+	
+	@Test
+	public void runCreateLead() {
+		
+		System.out.println("Starting Testcase Create Lead");
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+		System.out.println("Ending Testcase Create Lead");
+		
+}
+}
+
+
+
+
+
+
